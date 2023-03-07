5e4ffe28ecc150db7a5ad288b3c76485db5d2546
HAV47 <127195494+HAV47@users.noreply.github.com>
2023-03-07 17:44:23 +0700

    Add files via upload
    creat card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hải';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100079156995583';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Em';
+const contentCard = 'Chúc em ngày 8-3 vui vẻ, luôn hạnh phúc và giữ nụ cười trên môi♥ ♥ ♥ ♥ From : Fan hâm mộ giấu tên';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anhdep.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
